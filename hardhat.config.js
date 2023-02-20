10365032b51afb99aaec18e0a072018529a38416
toshitehdev <toshitehdev@proton.me>
2023-02-20 18:41:47 +0800

    add etherscan verification method

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const GEORLI_RPC_URL = process.env.GEORLI_RPC_URL;
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
 const PRIV_KEY = process.env.PRIV_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     goerli: {
-      url: GEORLI_RPC_URL,
+      url: GOERLI_RPC_URL,
       accounts: [PRIV_KEY],
       chainId: 5,
     },
   },
   solidity: "0.8.8",
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
